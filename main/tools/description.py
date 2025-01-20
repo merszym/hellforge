70a09f6d6f9408f5c18d69207e853c06ae0da757
Merlin Szymanski <merlin.szymanski@gmail.com>
2025-01-20 15:19:49 +0100

    fix opt-out querie

--- main/tools/description.py
+++ main/tools/description.py
@@ -26,7 +26,9 @@ def combine_pdf(request):
 
     project = get_project(request)
     # get all descriptions that are part of the sites in the project
-    descriptions = [Description.objects.get(project=project, site=x, exclude_from_print=False) for x in Site.objects.filter(project=project)]
+    descriptions = [Description.objects.get(project=project, site=x) for x in Site.objects.filter(project=project)]
+    # filter the descriptions that are opted out
+    descriptions = [x for x in descriptions if x.exclude_from_print==False]
     
     def pdf_generator():
         merger = PdfWriter()
